f2f375206316a41efd490f8b4f3d793d929bd753
megatron <megatron@megatrondeiMac.local>
2021-05-30 21:59:38 +0800

    adjust

---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jroimartin/gocui"
+	"log"
+	"sync"
+	"time"
+)
+
+const (
+	CellWidth int = 2
+	CellHeight int = 1
+
+	Tick = 50 * time.Millisecond
+)
+
+var (
+	done = make(chan struct{})
+	mainWG sync.WaitGroup
+)
+
+func main() {
+	// 初始化全局对象
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	// 初始化地图
+	g.SetManagerFunc(layout)
+
+	// 键盘事件响应
+	if err := keybindings(g); err != nil{
+		log.Panicln(err)
+	}
+
+	// 开始
+	snakeSetup()
+	mainWG.Add(1)
+	go snakeRun(g, &mainWG)
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+	mainWG.Wait()
+}
+
+func snakeRun(g *gocui.Gui, wg *sync.WaitGroup){
+	defer wg.Done()
+	for {
+		select {
+			case <- done:
+				return
+			case <- time.After(Tick):
+				g.Update(func(gui *gocui.Gui) error {
+					v, err := g.View("main")
+					if err != nil {
+						return err
+					}
+
+					v.Clear()
+					//fmt.Printf("resource: %c\n", snakeRecord[15][10])
+					for x:=0;x<MaxX;x++{
+						for y:=0;y<MaxY;y++{
+							if snakeRecord[x][y] == '#' {
+								fmt.Fprintf(v, "%s", "+")
+							}else{
+								fmt.Fprintf(v, "%s", "*")
+							}
+							if y % MaxY == 0 && x != 0{
+								fmt.Fprintf(v, "\n")
+							}
+						}
+					}
+					return nil
+				})
+		}
+	}
+}
+
+func keybindings(g *gocui.Gui) error {
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+	if err := g.SetKeybinding("", gocui.KeyArrowLeft, gocui.ModNone, MoveLeft); err != nil {
+		log.Panicln(err)
+	}
+	if err := g.SetKeybinding("", gocui.KeyArrowRight, gocui.ModNone, MoveRight); err != nil {
+		log.Panicln(err)
+	}
+	if err := g.SetKeybinding("", gocui.KeyArrowUp, gocui.ModNone, MoveUp); err != nil {
+		log.Panicln(err)
+	}
+	if err := g.SetKeybinding("", gocui.KeyArrowDown, gocui.ModNone, MoveDown); err != nil {
+		log.Panicln(err)
+	}
+	return nil
+}
+
+func layout(g *gocui.Gui) error {
+	//maxX, maxY := g.Size()
+	if _, err := g.SetView("main", 0, 0, MaxX * 0.7, MaxY * 0.6); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+	}
+
+	return nil
+}
+
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	close(done)
+	return gocui.ErrQuit
+}
---
+++ snake.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jroimartin/gocui"
+	"sync"
+)
+
+const (
+	MaxX = 50
+	MaxY = 35
+	SnakeLength = 3
+)
+
+
+
+type body struct {
+	X int
+	Y int
+}
+
+var (
+	snakeLock		sync.Mutex
+	snake			body
+	snakeRecord		[MaxX][MaxY] byte
+	snakeList		[]body
+)
+
+func snakeSetup(){
+	snakeLock.Lock()
+	defer snakeLock.Unlock()
+	snake.X = 10
+	snake.Y = 15
+	snakeLoad(snake)
+}
+
+func snakeLoad(b body){
+	fmt.Print("snakeLoad...")
+	//for x :=10;x<15;x++{
+	//	snake[10][x] = '#'
+	//}
+	for x := b.X;x < b.X+SnakeLength;x++{
+		snakeRecord[b.Y][x] = '#'
+		snakeList = append(snakeList, body{b.Y, x})
+	}
+
+	//fmt.Printf("snakeRecord: %c", snakeRecord[15][10])
+}
+
+func MoveLeft(g *gocui.Gui, v *gocui.View) error {
+	snakeLock.Lock()
+	defer snakeLock.Unlock()
+	length := len(snakeList)
+	headBody := snakeList[0]
+	lastBody := snakeList[length-1]
+	newItem := body{headBody.X, headBody.Y-1}
+	snakeList = append(snakeList[:length-1], newItem)
+
+	snakeRecord[headBody.X][headBody.Y-1] = '#'
+	snakeRecord[lastBody.X][lastBody.Y] = '*'
+	return nil
+}
+
+func MoveRight(g *gocui.Gui, v *gocui.View) error {
+	snakeLock.Lock()
+	defer snakeLock.Unlock()
+
+
+
+	return nil
+}
+
+func MoveUp(g *gocui.Gui, v *gocui.View) error {
+	snakeLock.Lock()
+	defer snakeLock.Unlock()
+
+
+
+	return nil
+}
+
+func MoveDown(g *gocui.Gui, v *gocui.View) error {
+	snakeLock.Lock()
+	defer snakeLock.Unlock()
+
+
+
+	return nil
+}
